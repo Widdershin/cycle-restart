b71469709774a32cf7d4a6e79b7e39b08b1a09d2
Nick Johnstone <ncwjohnstone@gmail.com>
2016-01-31 11:56:52 +1300

    Add observable event log w/ specs

--- test/node/restartable-test.js
+++ test/node/restartable-test.js
@@ -39,5 +39,78 @@ describe('restartable', () => {
       assert.deepEqual(loggedEvent.identifier, ':root');
       assert.deepEqual(loggedEvent.event, 'foo');
     });
+
+    it('is subscribable', (done) => {
+      const testSubject = new Subject();
+      const testDriver = () => testSubject;
+
+      const driver = restartable(testDriver)();
+
+      driver.log$.subscribe(log => {
+        assert.deepEqual(log, []);
+        done();
+      });
+    });
+
+    it('emits updates', (done) => {
+      const testSubject = new Subject();
+      const testDriver = () => testSubject;
+
+      const driver = restartable(testDriver)();
+
+      driver.log$.take(1).subscribe(log => {
+        assert.deepEqual(log, []);
+      });
+
+      driver.log$.skip(1).take(1).subscribe(log => {
+        const loggedEvent = log[0];
+
+        assert.deepEqual(loggedEvent.identifier, ':root');
+        assert.deepEqual(loggedEvent.event, 'foo');
+        done();
+      });
+
+      testSubject.onNext('foo');
+    });
+
+    it('shares the last value upon subscription', (done) => {
+      const testSubject = new Subject();
+      const testDriver = () => testSubject;
+
+      const driver = restartable(testDriver)();
+
+      testSubject.onNext('foo');
+      testSubject.onNext('foo');
+
+      driver.log$.subscribe(log => {
+        assert.equal(log.length, 2);
+        done();
+      });
+    });
+
+    it('handles selector style drivers', (done) => {
+      const testSubject = new Subject();
+      const testDriver = () => {
+        return {
+          select: (foo) => testSubject
+        };
+      };
+
+      const driver = restartable(testDriver)();
+
+      driver.select('snaz');
+
+      testSubject.onNext('foo');
+      testSubject.onNext('foo');
+
+      driver.log$.subscribe(log => {
+        assert.equal(log[0].identifier, 'snaz');
+        assert.equal(log[0].event, 'foo');
+
+        assert.equal(log.length, 2);
+
+        done();
+      });
+    });
   });
 });
