7d328f2cb9b53b4c735bb5481a0094a44c9af9d8
Nick Johnstone <ncwjohnstone@gmail.com>
2017-02-04 20:33:47 +1300

    Use superagent-mock instead of http server in node tests

--- test/node/http-test.js
+++ test/node/http-test.js
@@ -1,9 +1,11 @@
 /* globals describe, it, before, after */
 import assert from 'assert';
 import run from '@cycle/xstream-run';
-import {makeHTTPDriver} from '@cycle/http';
 import delay from 'xstream/extra/delay';
 
+const request = require('superagent');
+
+import {makeHTTPDriver} from '@cycle/http';
 const http = require('http');
 
 import {rerunner, restartable} from '../../src/restart';
@@ -12,41 +14,47 @@ import xs from 'xstream';
 
 let requestCount = 0;
 
-const server = http.createServer(function (req, res) {
-  requestCount += 1;
+const config = [
+  {
+    pattern: '/a',
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world! - ' + requestCount);
-});
+    fixtures: function (match) {
+      requestCount += 1;
 
-const PORT = 8532;
+      return 'Hello, world! - ' + requestCount;
+    },
 
-server.listen(PORT);
+    get: function (match, data) {
+      return {text: data};
+    }
+  }
+];
+
+const superagentMock = require('superagent-mock')(request, config);
 
 describe('restarting a cycle app that makes http requests', () => {
-  before(() => server.listen(PORT));
-  after(() => server.close());
-
-  function main ({HTTP}) {
+  function main ({HTTP, Start}) {
     const responses$ = HTTP.select().flatten().map(res => res.text);
 
-    responses$.addListener({next () {}}); // TODO - fix this
-
     return {
-      HTTP: xs.of('localhost:8532/a'),
+      HTTP: Start.mapTo('/a'),
       responses$: responses$
     };
   }
 
   it('only makes requests the first time', (done) => {
+    const start$ = xs.create();
+
     const driversFn = () => ({
-      HTTP: restartable(makeHTTPDriver({eager: true}))
+      HTTP: restartable(makeHTTPDriver()),
+      Start: () => start$
     });
 
     assert.equal(requestCount, 0);
 
     let rerun = rerunner(run, driversFn);
     rerun(main, () => {
+      start$.shamefullySendNext();
 
       setTimeout(() => {
         assert.equal(
@@ -54,13 +62,19 @@ describe('restarting a cycle app that makes http requests', () => {
           `Expected requestCount to be 1 prior to restart, was ${requestCount}.`
         );
 
+        let called = 0;
+
         rerun(main, () => {
           assert.equal(
             requestCount, 1,
             `Expected requestCount to be 1 after restart, was ${requestCount}.`
           );
 
-          done();
+          called++;
+
+          if (called === 1) {
+            done();
+          }
         });
       }, 20);
     });
@@ -69,8 +83,11 @@ describe('restarting a cycle app that makes http requests', () => {
   it('replays responses', (done) => {;
     requestCount = 0;
 
+    const start$ = xs.create();
+
     const driversFn = () => ({
-      HTTP: restartable(makeHTTPDriver())
+      HTTP: restartable(makeHTTPDriver()),
+      Start: () => start$
     });
 
     assert.equal(requestCount, 0);
@@ -85,11 +102,11 @@ describe('restarting a cycle app that makes http requests', () => {
     });
 
     sinks.responses$.take(1).addListener(goodListener(text => {
-      assert.equal(text, 'Hello, world! - 1');
+      assert.equal(text, 'Hello, world! - 2');
 
       assert.equal(
-        requestCount, 1,
-        `Expected requestCount to be 1 prior to restart, was ${requestCount}.`
+        requestCount, 2,
+        `Expected requestCount to be 2 prior to restart, was ${requestCount}.`
       );
 
       const restartedSinks = rerun(main).sinks;
@@ -97,20 +114,22 @@ describe('restarting a cycle app that makes http requests', () => {
       restartedSinks.responses$.take(1).addListener(goodListener(text => {
 
         assert.equal(
-          requestCount, 1,
-          `Expected requestCount to be 1 after restart, was ${requestCount}.`
+          requestCount, 2,
+          `Expected requestCount to be 2 after restart, was ${requestCount}.`
         );
 
         done();
       }));
     }));
+
+    start$.shamefullySendNext('');
   });
 
   function requestMain ({HTTP}) {
     const request$ = HTTP.select().flatten().map(res$ => res$.request)
 
     return {
-      HTTP: xs.of('localhost:8532/a').compose(delay(50)),
+      HTTP: xs.of('/a').compose(delay(50)),
       request$
     };
   }
@@ -136,12 +155,12 @@ describe('restarting a cycle app that makes http requests', () => {
     const {sinks} = rerun(requestMain);
 
     sinks.request$.take(1).addListener(goodListener(text => {
-      assert.equal(text.url, 'localhost:8532/a');
+      assert.equal(text.url, '/a');
 
       const restartedSinks = rerun(requestMain).sinks;
 
       restartedSinks.request$.take(1).addListener(goodListener(text => {
-        assert.equal(text.url, 'localhost:8532/a');
+        assert.equal(text.url, '/a');
 
         done();
       }));
